aef1a422088714023782d85da8df011478e4b43c
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-05 23:06:12 +0900

    Update prime.ml

--- example/prime.ml
+++ example/prime.ml
@@ -1,29 +1,29 @@
 let solveDivisors n =
 begin
-  let divisorList = [];
-  for i as num =
-    1 ~ (n + 1) do
-  begin
-    let remainder = n % i;
-    if remainder == 0 then
-      divisorList += [i]
-  end;
-  divisorList
+    let divisorList = [];
+    for i as num =
+        1 ~ (n + 1) do
+    begin
+        let remainder = n % i;
+        if remainder == 0 then
+            divisorList += [i]
+    end;
+    divisorList
 end;
 
 print "Primality checker\n";
 effect while let n =
-  input ">> " as num do
+    input ">> " as num do
 begin
-  print
-  if let divisors =
-    solveDivisors n then
-  begin
-    if divisors == [1, n]
-      then f"TRUE: {n} is prime number"
-      else f"FALSE: {n} is diviable by {divisors}"
-  end
-  else
-    "ERROR: input should be greater than 1"
-  , "\n" * 2
+    print
+    if let divisors =
+        solveDivisors n then
+    begin
+        if divisors == [1, n]
+            then f"TRUE: {n} is prime number"
+            else f"FALSE: {n} is diviable by {divisors}"
+    end
+    else
+        "ERROR: input should be greater than 1"
+    , "\n" * 2
 end
