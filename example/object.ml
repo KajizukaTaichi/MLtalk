1364253befd84423596810b20107a1e2af4f2f41
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-05 21:22:37 +0900

    Update object.ml

--- example/object.ml
+++ example/object.ml
@@ -5,7 +5,7 @@ let Dog = {
 };
 
 let Dog.new name = Dog + { name };
-let Dog.say this =
+effect let Dog.say this =
   print f"{this.name} the {type this} says \"{this.voice}, {this.voice}!\"\n";
 
 let Cat = Dog + {
@@ -14,12 +14,12 @@ let Cat = Dog + {
 };
 
 let Cat.new name = Cat + { name };
-let Cat.punch this power =
-  for i in 0 ~ power do
+effect let Cat.punch this power =
+  for i = 0 ~ power do
     print f"Thud! {this.name} sends out cat punch\n";
 
 let pochi = Dog.new "Pochi";
-pochi say;
+effect pochi say;
 
 let tama = Cat.new "Tama";
-tama say; tama punch 3;
+effect tama say; effect tama punch 3;
