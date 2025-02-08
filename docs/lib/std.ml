1173fb4f0a2b78e4d11907e9afccfb942b0d5d65
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-08 15:50:34 +0900

    Update std.ml

--- docs/lib/std.ml
+++ docs/lib/std.ml
@@ -1,71 +1,71 @@
 let length x =
 begin
-  let i = 0;
-  for _ = x do
-    i += 1;
-  i
+    let i = 0;
+    for _ = x do
+        i += 1;
+    i
 end;
 
 let List = {};
 
 let List.map f x =
 begin
-  let result = [];
-  for i = x do
-    result += [f i];
-  result
+    let result = [];
+    for i = x do
+        result += [f i];
+    result
 end;
 
 let List.filter f x =
 begin
-  let result = [];
-  for i = x do
-    if f i then
-      result += [i];
-  result
+    let result = [];
+    for i = x do
+        if f i then
+            result += [i];
+    result
 end;
 
 let List.reduce f x =
 begin
-  let result = x :: 0;
-  let index as num = 1;
-  while let elm = x[index] do
-  begin
-    let result = f result elm;
-    index += 1
-  end;
-  result
+    let result = x :: 0;
+    let index as num = 1;
+    while let elm = x[index] do
+    begin
+        let result = f result elm;
+        index += 1
+    end;
+    result
 end;
 
 let List.zip lists =
 begin
-  let [index, result] = [0, []];
-  while let elms =
-    List.map \x.x[index] lists do
-  begin
-    result += [elms];
-    index += 1
-  end;
-  result
+    let [index, result] = [0, []];
+    while let elms =
+        List.map \x.x[index] lists do
+    begin
+        result += [elms];
+        index += 1
+    end;
+    result
 end;
 
 let List.join x s =
 begin
-  let result = "";
-  let xlen = length x;
-  for i = x do
-  begin
-    result += (i as str);
-    if x[[i]] != (xlen - 1 ~ xlen)
-      then result += s
-  end;
-  result
+    let result = "";
+    let xlen = length x;
+    for i = x do
+    begin
+        result += (i as str);
+        if x[[i]] != (xlen - 1 ~ xlen)
+            then result += s
+    end;
+    result
 end;
 
 let List.reverse x =
 begin
-  let result = [];
-  for i = x do
-    result := ([i] + result);
-  result
+    let result = [];
+    for i = x do
+        result := ([i] + result);
+    result
 end
