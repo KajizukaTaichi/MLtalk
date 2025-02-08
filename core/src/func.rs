ce20e9e62fa6cb67dc6ecfc11be18697671eaf9e
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-08 14:26:55 +0900

    Update func.rs

--- core/src/func.rs
+++ core/src/func.rs
@@ -11,21 +11,25 @@ impl Func {
         // Lambda abstract that original formula in the theory
         if source.starts_with("λ") && source.contains(".") {
             let source = remove!(source, "λ");
-            let (arg, body) = ok!(source.split_once("."))?;
-            Ok(Func::UserDefined(
-                arg.to_string(),
-                Box::new(Expr::parse(body)?),
-            ))
+            Self::common(&source)
         // Lambda abstract using back-slash instead of lambda mark
         } else if source.starts_with("\\") && source.contains(".") {
             let source = remove!(source, "\\");
-            let (arg, body) = ok!(source.split_once("."))?;
-            Ok(Func::UserDefined(
-                arg.to_string(),
-                Box::new(Expr::parse(body)?),
-            ))
+            Self::common(&source)
         } else {
             Err(Fault::Syntax)
         }
+    }
+
+    fn common(source: &str) -> Result<Self, Fault> {
+        let (arg, body) = ok!(source.split_once("."))?;
+        let arg = arg.trim();
+        if arg.is_empty() {
+            return Err(Fault::Syntax);
+        }
+        Ok(Func::UserDefined(
+            arg.to_string(),
+            Box::new(Expr::parse(body)?),
+        ))
     }
 }
