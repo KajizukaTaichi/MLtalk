6f6d7174174cc3617f271f7960706e77012133e3
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-11 14:55:11 +0900

    Update prime.ml

--- example/prime.ml
+++ example/prime.ml
@@ -1,14 +1,22 @@
 effect load stdlib;
-let solveDivisors n = List.filter (\x. n % x == 0) (1~(n+1) as list);
 
-effect print "Primality checker\n";
-while let n = input ">> " as num do
+let solveDivisors n = List.filter (\x. n % x == 0) (1~(n+1) as list);
+bind solveDivisors = fn(num -> list);
+
+effect let mainLoop _ =
 begin
-    if let divisors = solveDivisors n then
-    effect print
+    print "Primality checker\n";
+    while let n = input ">> " as num do
     begin
-        if divisors == [1, n]
-            then f"TRUE: {n} is prime number\n"
-            else f"FALSE: {n} is diviable by {divisors}\n"
+        if let divisors =
+            solveDivisors n then
+        print
+        begin
+            if divisors == [1, n]
+                then f"TRUE: {n} is prime number\n"
+                else f"FALSE: {n} is diviable by {divisors}\n"
+        end
+        else
+            print "ERROR: input should be greater than 1\n"
     end
-end
+end;
