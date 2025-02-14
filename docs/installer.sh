46c6b31f292dc57bdc7ffdb19ffaf63865f21078
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-14 16:58:59 +0900

    Update installer.sh

--- docs/installer.sh
+++ docs/installer.sh
@@ -2,7 +2,7 @@ printf "Welcome to the \e[1;34mMLtalk\e[0m programming language!\n"
 printf "[\e[1;34minfo\e[0m] cloning repository from GitHub\n"
 git clone https://github.com/KajizukaTaichi/MLtalk.git
 printf "[\e[1;34minfo\e[0m] Building then add to the PATH\n"
-cargo install --path ./Mltalk/repl
+cargo install --path ./MLtalk/repl
 printf "[\e[1;34minfo\e[0m] removing build directory\n"
-rm -rf ./Mltalk
+rm -rf ./MLtalk
 printf "[\e[1;34minfo\e[0m] installing is finished successfully\n"
