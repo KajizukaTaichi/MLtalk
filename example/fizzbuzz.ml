684ac4ae58a6b9ac827865d09af71d147dc997a6
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-16 10:48:08 +0900

    Update fizzbuzz.ml

--- example/fizzbuzz.ml
+++ example/fizzbuzz.ml
@@ -7,8 +7,8 @@ begin
     else if divisible 3 then "Fizz"
     else if divisible 5 then "Buzz"
     else n
-end;
-bind getFizzbuzz = fn(num -> num | str);
+end
+;bind getFizzbuzz = fn(num -> num | str);
 
 lazy let fizzbuzzList = List.map getFizzbuzz (1~101 as list);
 effect print (List.join fizzbuzzList "\n")
