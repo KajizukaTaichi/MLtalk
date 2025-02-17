0ed8ad75a869da970cda5c898942c18455f9513e
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-17 20:08:23 +0900

    Update fizzbuzz.ml

--- example/fizzbuzz.ml
+++ example/fizzbuzz.ml
@@ -1,13 +1,11 @@
 effect load stdlib;
 
 let getFizzbuzz n =
-begin
-    let divisible = (\x. n % x == 0);
-    if divisible 15 then "FizzBuzz"
-    else if divisible 3 then "Fizz"
-    else if divisible 5 then "Buzz"
-    else n
-end;
+match [n % 3, n % 5] with
+    [0, 0] => "FizzBuzz",
+    [0, _] => "Fizz",
+    [_, 0] => "Buzz",
+    [_, _] => n;
 bind getFizzbuzz = fn(num -> num | str);
 
 lazy let fizzbuzzList = List.map getFizzbuzz (1~101 as list);
