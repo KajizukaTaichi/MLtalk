9d46d2d1b275faae8e9f31e3ec8f15a56debd65a
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-17 16:44:15 +0900

    Update lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,27 @@
-use mltalk_core::{Block, Engine, Node};
+use mltalk_core::{Block, Engine, Fault, Func, Node, Value};
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = globalThis)]
+    fn eval(js_code: &str) -> JsValue;
+}
 
 #[wasm_bindgen]
 pub struct MLtalk {
     engine: Engine,
+}
+
+fn jsvalue_to_mltalk(result: JsValue) -> Result<Value, Fault> {
+    if result.is_null() {
+        Ok(Value::Null)
+    } else if let Some(n) = result.as_f64() {
+        Ok(Value::Num(n))
+    } else if let Some(s) = result.as_string() {
+        Ok(Value::Str(s))
+    } else {
+        Err(Fault::IO)
+    }
 }
 
 #[wasm_bindgen]
@@ -11,7 +29,22 @@ impl MLtalk {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         MLtalk {
-            engine: Engine::new(),
+            engine: {
+                let mut engine = Engine::new();
+                let _ = engine.alloc(
+                    &"jsEval".to_string(),
+                    &Value::Func(Func::BuiltIn(|arg, _| {
+                        let code = &arg.get_str()?;
+                        if let Ok(result) = std::panic::catch_unwind(|| eval(code)) {
+                            Ok(jsvalue_to_mltalk(result)?)
+                        } else {
+                            Err(Fault::IO)
+                        }
+                    })),
+                );
+                engine.set_effect("jsEval");
+                engine
+            },
         }
     }
 
