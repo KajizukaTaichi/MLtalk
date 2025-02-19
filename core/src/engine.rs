562e670194690ab561a73094fec5abed85f6a528
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-19 13:55:30 +0900

    Update engine.rs

--- core/src/engine.rs
+++ core/src/engine.rs
@@ -3,7 +3,6 @@ use crate::*;
 #[derive(Debug, Clone)]
 pub struct Engine {
     pub scope: IndexMap<String, Value>,
-    pub effect: IndexSet<String>,
     pub is_toplevel: bool,
     pub is_lazy: bool,
     pub mode: Mode,
@@ -21,7 +20,6 @@ impl Engine {
             mode: Mode::Pure,
             is_toplevel: true,
             is_lazy: false,
-            effect: IndexSet::from(["eval".to_string()]),
             scope: IndexMap::from([
                 (
                     "stdlib".to_string(),
@@ -29,38 +27,57 @@ impl Engine {
                 ),
                 (
                     "eval".to_string(),
-                    Value::Func(Func::BuiltIn(|expr, engine| {
-                        Ok(Block::parse(&expr.get_str()?)?.eval(engine)?)
-                    })),
+                    Value::Func(Func::BuiltIn(
+                        |expr, engine| Ok(Block::parse(&expr.get_str()?)?.eval(engine)?),
+                        Type::Func(Some(Box::new((Type::Str, Type::Any))), Mode::Effect),
+                    )),
                 ),
                 (
                     "type".to_string(),
-                    Value::Func(Func::BuiltIn(|expr, _| Ok(Value::Type(expr.type_of())))),
+                    Value::Func(Func::BuiltIn(
+                        |expr, _| Ok(Value::Type(expr.type_of())),
+                        Type::Func(Some(Box::new((Type::Any, Type::Kind))), Mode::Pure),
+                    )),
                 ),
                 (
                     "alphaConvert".to_string(),
-                    Value::Func(Func::BuiltIn(|args, _| {
-                        let args = args.get_list()?;
-                        let func = ok!(args.first(), Fault::ArgLen)?;
-                        let new_name = ok!(args.get(1), Fault::ArgLen)?.get_str()?;
-                        let Value::Func(Func::UserDefined(old_name, body, anno)) = func else {
-                            return Err(Fault::Type(func.to_owned(), Type::Func(None, Mode::Pure)));
-                        };
-                        Ok(Value::Func(Func::UserDefined(
-                            new_name.clone(),
-                            Box::new(body.replace(
-                                &Expr::Refer(old_name.to_owned()),
-                                &Expr::Refer(new_name),
-                            )),
-                            anno.clone(),
-                        )))
-                    })),
+                    Value::Func(Func::BuiltIn(
+                        |args, _| {
+                            let args = args.get_list()?;
+                            let func = ok!(args.first(), Fault::ArgLen)?.get_func()?;
+                            let new_name = ok!(args.get(1), Fault::ArgLen)?.get_str()?;
+                            let Func::UserDefined(old_name, body, anno) = func else {
+                                return Err(Fault::Type(
+                                    Value::Func(func.to_owned()),
+                                    Type::Func(None, Mode::Pure),
+                                ));
+                            };
+                            Ok(Value::Func(Func::UserDefined(
+                                new_name.clone(),
+                                Box::new(body.replace(
+                                    &Expr::Refer(old_name.to_owned()),
+                                    &Expr::Refer(new_name),
+                                )),
+                                anno.clone(),
+                            )))
+                        },
+                        Type::Func(
+                            Some(Box::new((
+                                Type::List(Some(Box::new(Type::Union(vec![
+                                    Type::Func(None, Mode::Pure),
+                                    Type::Str,
+                                ])))),
+                                Type::Func(None, Mode::Pure),
+                            ))),
+                            Mode::Pure,
+                        ),
+                    )),
                 ),
             ]),
         }
     }
 
-    pub fn alloc(&mut self, name: &String, value: &Value) -> Result<(), Fault> {
+    pub fn malloc(&mut self, name: &String, value: &Value) -> Result<(), Fault> {
         if is_identifier(name) {
             if name != "_" {
                 self.scope.insert(name.clone(), value.clone());
@@ -72,30 +89,15 @@ impl Engine {
     }
 
     pub fn access(&mut self, name: &str) -> Result<Value, Fault> {
-        ok!(
-            if let Mode::Pure = self.mode {
-                if self.is_effective(name) {
-                    return Err(Fault::Pure(name.to_string()));
-                } else {
-                    self.scope.get(name)
-                }
+        let val = ok!(self.scope.get(name), Fault::Refer(name.to_owned()))?.clone();
+        if let Mode::Pure = self.mode {
+            if Expr::Value(val.clone()).is_pure() {
+                Ok(val)
             } else {
-                self.scope.get(name)
-            },
-            Fault::Refer(name.to_owned())
-        )
-        .cloned()
-    }
-
-    pub fn set_effect(&mut self, name: &str) {
-        self.effect.insert(name.to_string());
-    }
-
-    pub fn unset_effect(&mut self, name: &str) {
-        self.effect.shift_remove(name);
-    }
-
-    pub fn is_effective(&self, name: &str) -> bool {
-        self.effect.contains(&name.to_string())
+                Err(Fault::Pure(name.to_string()))
+            }
+        } else {
+            Ok(val)
+        }
     }
 }
