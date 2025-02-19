07916addfd055b489df5486ace927fc013c27f8a
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-19 14:42:46 +0900

    Update node.rs

--- core/src/node.rs
+++ core/src/node.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 pub trait Node {
     fn eval(&self, engine: &mut Engine) -> Result<Value, Fault>;
-    fn is_pure(&self) -> bool;
+    fn is_pure(&self, engine: &mut Engine) -> bool;
     fn replace(&self, from: &Expr, to: &Expr) -> Self;
     fn parse(source: &str) -> Result<Self, Fault>
     where
