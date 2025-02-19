a85d8f6b288aef05c3627c315fd905ed1159fff6
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-19 14:31:59 +0900

    Update engine.rs

--- core/src/engine.rs
+++ core/src/engine.rs
@@ -105,7 +105,7 @@ impl Engine {
 
     pub fn effect_collection(&mut self) {
         for (name, value) in &self.scope {
-            if Expr::Value(value.clone()).is_pure() {
+            if !Expr::Value(value.clone()).is_pure() {
                 self.effective.insert(name.to_string());
             }
         }
