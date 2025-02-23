08b2c5be3284153cf9cd0cbbc22c8d2e765d3d62
梶塚太智 <kajizukataichi@outlook.jp>
2025-02-23 12:51:34 +0900

    Update node.rs

--- core/src/node.rs
+++ core/src/node.rs
@@ -4,7 +4,7 @@ pub trait Node {
     fn eval(&self, engine: &mut Engine) -> Result<Value, Fault>;
     fn is_pure(&self, engine: &Engine) -> bool;
     fn replace(&self, from: &Expr, to: &Expr) -> Self;
-    fn infer(&self, engine: &Engine) -> Type;
+    fn infer(&self, engine: &Engine) -> Option<Type>;
     fn parse(source: &str) -> Result<Self, Fault>
     where
         Self: Node + Sized;
